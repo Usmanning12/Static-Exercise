193a5c349cd1d536d0f701e692432cb6f3ba262c
Darron Manning <darronmanning@yahoo.com>
2025-07-03 16:17:03 -0500

    StaticExercise

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -1,10 +1,15 @@
 ﻿namespace StaticExercise
 {
-    public class Program
+    public  static class program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("After Conversion");  
+            var celsius = TempConverter.FahrenheitToCelsius(68);
+            Console.WriteLine($"Celsius {celsius}");
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(20);
+            Console.WriteLine($"Fahrenheit {fahrenheit}");
+            
         }
     }
 }
